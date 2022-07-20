0007678384980adb187f95bfd8fe37d541fadd1e
Бойченко Вадим Валерьевич <vadim.boychenko@gmail.com>
2022-07-20 21:35:17 +0300

    80% ready

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,33 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
     //новый тест гита
 
-    
+    public static Connection getMySQLConnection() throws SQLException {
+        String hostName = "localhost";
+
+        String dbName = "userdb";
+        String userName = "root";
+        String password = "SoStrong#256";
+
+        return getMySQLConnection(hostName, dbName, userName, password);
+
+    }
+
+    public static Connection getMySQLConnection(String hostName, String dbName,
+                                                String userName, String password) throws SQLException
+            {
+
+
+        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+
+        Connection conn = DriverManager.getConnection(connectionURL, userName, password);
+        return conn;
+    }
+
 }
